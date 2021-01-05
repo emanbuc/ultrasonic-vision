4ba15d0372d21d1410036d2045eab2e93b04395f
Emanuele Buchicchio <emanuelebuchicchio@iMac-di-Emanuele.local>
2021-01-05 22:23:16 +0100

    feat: enpoint test with autentication from args

--- src/test_endpoint.py
+++ src/test_endpoint.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from json.decoder import JSONDecoder
 import sys, getopt
 
 # URL for the web service, should be similar to:
@@ -9,73 +10,68 @@ scoring_uri = 'http://64b32d7c-d926-4197-b807-1350e63adf7c.westeurope.azureconta
 # If the service is authenticated, set the key or token
 key = ''
 
+SENSORS= ['HCSR04_001','HCSR04_002','HCSR04_003','HCSR04_004','HCSR04_005','HCSR04_006','HCSR04_007'] #List os sensors unique ID
 # Two sets of data to score, so we get two results back
-data = {"data":
-        [
-          {
-            "Time": 0,
-            "HCSR04_001": 20,
-            "HCSR04_002": 20,
-            "HCSR04_003": 20,
-            "HCSR04_004": 20,
-            "HCSR04_005": 20,
-            "HCSR04_006": 20,
-            "HCSR04_007": 20
-          },
-          {
-            "Time": 0,
-            "HCSR04_001": 10,
-            "HCSR04_002": 60,
-            "HCSR04_003": 60,
-            "HCSR04_004": 40,
-            "HCSR04_005": 50,
-            "HCSR04_006": 30,
-            "HCSR04_007": 60
-          }
-      ]
-    }
+rawData = {"Time": 0}
+#EMPTY_SEVEN
+rawData['HCSR04_001'] = 54.2
+rawData['HCSR04_002'] = 55.0
+rawData['HCSR04_003'] = 55.3
+rawData['HCSR04_004'] = 53.2
+rawData['HCSR04_005'] = 48.9
+rawData['HCSR04_006'] = 48.8
+rawData['HCSR04_007'] = 49.3
+            
+          #BENA_CAN
+#         {
+#             "Time": 0,
+#             "HCSR04_001": 20.3,
+#             "HCSR04_002": 15.9,
+#             "HCSR04_003": 17.9,
+#             "HCSR04_004": 19.6,
+#             "HCSR04_005": 28.2,
+#             "HCSR04_006": 40.6,
+#             "HCSR04_007": 92.8
+#           }
 
 def makeRequest(scoring_uri,key):
-  # Convert to JSON string
-  input_data = json.dumps(data)
-  with open("data.json", "w") as _f:
-      _f.write(input_data)
+    wsInputData ={'Column2': '0'}
+    wsInputData['distanceSumLow'] = rawData['HCSR04_001'] + rawData['HCSR04_002']
+    wsInputData['distanceSumHi'] = rawData['HCSR04_003'] + rawData['HCSR04_004']
+    wsInputData['differentialDistanceFromRoof65'] = rawData['HCSR04_006'] - rawData['HCSR04_005']
+    wsInputData['differentialDistanceFromRoof67'] = rawData['HCSR04_006'] - rawData['HCSR04_007']
+    wsInputData['differentialDistanceFromRoof57'] = rawData['HCSR04_005']- rawData['HCSR04_007']
+    
+    inputDataObj = {
+        "data": [wsInputData]
+    }
+    print("Input data:")
+    print(inputDataObj)
+    # Convert to JSON string
+    inputDataJson = json.dumps(inputDataObj)
 
-  # Set the content type
-  headers = {'Content-Type': 'application/json'}
-  # If authentication is enabled, set the authorization header
-  headers['Authorization'] = f'Bearer {key}'
+    # Set the content type
+    headers = {'Content-Type': 'application/json'}
+    # If authentication is enabled, set the authorization header
+    headers['Authorization'] = f'Bearer {key}'
 
-  # Make the request and display the response
-  resp = requests.post(scoring_uri, input_data, headers=headers)
-  print(resp.json())
+    # Make the request and display the response
+    resp = requests.post(scoring_uri, inputDataJson, headers=headers)
+    
+    #TODO: parse json result
+    respObj = JSONDecoder().decode(resp.json())
+    return respObj['result'][0]
 
 def main(argv,key,scoring_uri):
-   inputfile = ''
-   outputfile = ''
-   try:
-      opts, args = getopt.getopt(argv,"hi:o:k:u",["ifile=","ofile=","key","uri"])
-   except getopt.GetoptError:
-      print ('test.py -k <key> -u <uri> -i <inputfile> -o <outputfile>')
+   if len(argv)!= 2:
+      print("usage: ultrasonic-vision.py <key> <scoring-uri>")
       sys.exit(2)
-   for opt, arg in opts:
-      if opt == '-h':
-         print ('test.py -k <key> -u <uri> -i <inputfile> -o <outputfile>')
-         sys.exit()
-      elif opt in ("-i", "--ifile"):
-         inputfile = arg
-      elif opt in ("-o", "--ofile"):
-         outputfile = arg
-      elif opt in ("-k","--key"):
-        key= arg
-      elif opt in ("-u","--uri"):
-        scoring_uri= arg
 
-   print ('Input file is "', inputfile)
-   print ('Output file is "', outputfile)
-   print ('Key token is "', key)
-   print ('URI token is "', scoring_uri)
-   makeRequest(scoring_uri,key)
+   key = argv[0]
+   scoring_uri = argv[1]
+   objectClass= makeRequest(scoring_uri,key)
+   print("Output:")
+   print("Object Type: "+ objectClass)
 
 if __name__ == "__main__":
    main(sys.argv[1:],key,scoring_uri)
