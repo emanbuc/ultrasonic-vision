313521d1374ea03d83fb37e5533d902aa5bce859
Emanuele Buchicchio <emanuelebuchicchio@iMac-di-Emanuele.local>
2021-01-08 00:32:35 +0100

    feat: py script for Scikit Learn model conversion to ONNX

--- src/convertToONNX.py
+++ src/convertToONNX.py
@@ -1,29 +1,27 @@
 import pickle
 
 
-x_test= [59,87,26,30,35,40,80]
-x_test2= [60,87,25,40,80,90]
-filename = 'C:/gitrepos/ultrasonic-vision/models/linearSVC_classificator_model_pickle_outline_removed.pkl'
+x_test= [[59,87,26,30,35,40,80]]
+filename = '../models/rf_classificator_model_pickle_outline_removed.pkl'
 load_lr_model =pickle.load(open(filename, 'rb'))
-result =load_lr_model.predict([x_test])
+result =load_lr_model.predict(x_test)
 print(result)
 
 # Convert into ONNX format
 from skl2onnx import convert_sklearn
 from skl2onnx.common.data_types import FloatTensorType
 
-initial_type = [('float_input', FloatTensorType([None, 5]))]
+initial_type = [('float_input', FloatTensorType([None, 7]))]
 onx = convert_sklearn(load_lr_model, initial_types=initial_type)
-with open(filename+".onnx", "wb") as f:
-    f.write(onx.SerializeToString())
 
 # Compute the prediction with ONNX Runtime
 import onnxruntime as rt
-import numpy
+import numpy as np
+x_test_array = np.array(x_test, dtype=np.float32)
 
-x_test_array = numpy.array([20,30,40,50,20,30,40,50,10], dtype=numpy.float32)
-sess = rt.InferenceSession(filename+".onnx")
+sess = rt.InferenceSession(onx.SerializeToString())
+inputs = sess.get_inputs()[0].name
 input_name = sess.get_inputs()[0].name
-label_name = sess.get_outputs()[0].name
-pred_onx = sess.run([label_name], {input_name: x_test_array})[0]
+pred_onx = sess.run(None, {input_name: x_test_array})[0]
+
 print(pred_onx)
